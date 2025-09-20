2cc56721180f23949b26ec53812653c51e4db450
namnguyen <namhoanghainguyen@gmail.com>
2025-09-20 10:20:39 -0600

    first commit on weather calling

---
+++ src/main/java/com/example/WeatherRequestAPI/WeatherRequestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.WeatherRequestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherRequestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherRequestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/WeatherRequestAPI/WeatherRequestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WeatherRequestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherRequestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
